29759387cdb8ea08c8a688e4c0fd9ddef8dc869b
Lars Peter Jeppesen <larspeterjeppesen@gmail.com>
2025-03-13 20:24:48 +0100

    got insert function to compile. It's heavily mutable. need to implement search method next

--- wordbush/src/main.rs
+++ wordbush/src/main.rs
@@ -16,7 +16,7 @@ use std::collections::HashMap;
 // Informationen er et eller flere chars. Hvis man løber fra stammen ned til en struct, kan man danne et ord
 
 
-
+#[derive(Clone)]
 struct Radix {
     chars: String,
     children: HashMap<char, Radix>,
@@ -57,8 +57,8 @@ The word is guaranteed to fit there, as we are "building" the word as we look up
 
 
 
-fn insert(root: Radix, insertion_word: &str) -> Result<()> {
-    let next_node: Option<&Radix> = root.children.get(&insertion_word.chars().next().unwrap());
+fn insert(mut root: &mut Radix, insertion_word: String) -> Result<()> {
+    let next_node: Option<&mut Radix> = root.children.get_mut(&insertion_word.chars().next().unwrap());
     match next_node {
         None => {
             // no match, insert node
@@ -66,10 +66,13 @@ fn insert(root: Radix, insertion_word: &str) -> Result<()> {
         },
         Some(child) => {
             if insertion_word == child.chars {
-                println!("Word: {insertion_word} already exists in tree");
+                if !child.is_word  {
+                    // Child may not be recognized as a word until now, make sure it is
+                    child.is_word = true;
+                }
                 return Ok(());
             }
-            let common_prefix: &str = zip(child.chars.chars(), insertion_word.chars())
+            let common_prefix: String = zip(child.chars.chars(), insertion_word.chars())
                 .take_while(|(a,b)| a == b)
                 .map(|(a,_)| a)
                 .collect();
@@ -77,28 +80,27 @@ fn insert(root: Radix, insertion_word: &str) -> Result<()> {
             if common_prefix.len() < insertion_word.len() && common_prefix.len() < child.chars.len() {
                 // child must be split to be the common prefix of both words.
                 // the children of child must be set to the children of the node that will contain the suffix of child
-                let insertion_word_child = Radix {chars: insertion_word[common_prefix.len()..], children: HashMap::new(), is_word: true};
-                let new_child = Radix {chars: child.chars[common_prefix.len()..], children: child.children, is_word: child.is_word};
+                let insertion_word_child = Radix {chars: insertion_word[common_prefix.len()..].to_string(), children: HashMap::new(), is_word: true};
+                let new_child = Radix {chars: child.chars[common_prefix.len()..].to_string(), children: child.children.clone(), is_word: child.is_word};
 
-                let new_parent = Radix {chars: common_prefix, children: HashMap::from([(new_child[..1], new_child), (insertion_word_child[..1], insertion_word_child)])};                
+                let new_parent = Radix {chars: common_prefix, children: HashMap::from([(new_child.chars.chars().next().unwrap(), new_child), (insertion_word_child.chars.chars().next().unwrap(), insertion_word_child)]), is_word: false};  
+                root.children.insert(new_parent.chars.chars().next().unwrap(), new_parent);             
 
-                ()
+                return Ok(());
             }
 
             // alternatively insertion_word.len() == common_prefix.len()
             if insertion_word.len() < child.chars.len() {
                 // insert insertion_word as a parent of current_word
-                let new_child = Radix {chars: child.chars[common_prefix.len()..], children: child.children, is_word: child.is_word};
-                let parent = Radix {chars: common_prefix, children: HashMap::from([(new_child.chars[..1], new_child)]), is_word: true};
-                let _ = root.children.insert(insertion_word[..1], new_child);
-                ()
+                let new_child = Radix {chars: child.chars.chars().take(common_prefix.len()).collect(), children: child.children.clone(), is_word: child.is_word};
+                let parent = Radix {chars: common_prefix, children: HashMap::from([(new_child.chars.chars().next().unwrap(), new_child)]), is_word: true};
+                let _ = root.children.insert(insertion_word.chars().next().unwrap(), parent);
+                return Ok(());
             }
             else {
                 // Search further
-                insert(&child, insertion_word[common_prefix.len()..])
-                ()
+                return insert(child, insertion_word.chars().take(common_prefix.len()).collect());
             }
-            ()
 
         },
     }
